4c4ca35ce13fc1fe214b199122f9945832f7332c
Ben Brougher <moutansos@gmail.ocm>
2019-03-10 21:12:24 -0700

    added the basic bot connecting to the server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/moutansos/gofinder
 
 go 1.12
+
+require github.com/bwmarrin/discordgo v0.19.0
--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"log"
 	"fmt"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Starting Go Finder bot...")
+
+	discord, err := discordgo.New("Bot " + "<<API KEY>>")
+	if err != nil {
+		log.Fatal("Unable to connect to discord")
+	}
+	user, err := discord.User("@me")
+	if err != nil {
+		log.Fatal("Cant retireve the user for self:", err)
+	}
+	id := user.ID
+	log.Printf("User id is %s", id)
+
+	discord.AddHandler(initBot)
+
+	if err := discord.Open(); err != nil {
+		log.Fatal("Error opening discord:", err)
+	}
+	defer discord.Close()
+	
+	<-make(chan struct{})
+}
+
+func initBot(discord *discordgo.Session, ready *discordgo.Ready) {
+	err := discord.UpdateStatus(0, "I'm alive!")
+	if err != nil {
+		log.Fatal("Error setting my status: ", err)
+	}
+	servers := discord.State.Guilds
+	log.Println(fmt.Sprintf("Now connected to %d servers.", len(servers)))
 }
